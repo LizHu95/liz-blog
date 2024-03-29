f10fd4219bbc02c7ce76708603d15b0fcc5f6e22
LizHu95 <707628390@qq.com>
2024-03-29 14:07:21 +0800

    feat: init

---
+++ src/utils/AppConfig.ts
@@ -0,0 +1,8 @@
+export const AppConfig = {
+  site_name: 'Liz boilerplate',
+  title: 'Liz boilerplate',
+  description: 'Boilerplate built with Astro using React and Tailwind CSS',
+  author: 'Liz',
+  locale_region: 'en-us',
+  locale: 'en',
+};
